cc79da1c8bb1be72ae0460c02b0437d0bd60dee8
Rob McGrail <rob@cwa-1498.(none)>
2011-11-16 14:13:40 +1300

    error text for flashes

---
+++ helpers/error_text.rb
@@ -0,0 +1,9 @@
+helpers do
+  def error_text
+    {
+      :no_handlers => 'Serious error: Search handlers not found!',
+      :forbidden => 'You can\'t go there!',
+      :generic => 'An error has occured',
+    }
+  end
+end

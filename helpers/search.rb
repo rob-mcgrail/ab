a7373a95f809851467d89268c092f82eedb058bf
Rob McGrail <rob@cwa-1498.(none)>
2011-11-16 15:43:42 +1300

    helpers for search controllers

--- helpers/search.rb
+++ helpers/search.rb
@@ -5,4 +5,21 @@ helpers do
       redirect '/'
     end
   end
+  
+  def injest_query
+    if params[:query_term] == nil || params[:query_term] == ''
+      flash[:error] = error_text[:no_query]
+      redirect '/'
+    end
+    params[:query_term]
+  end
+  
+  def ab_search(handlers, q)
+    results = []
+    handlers.each do |k,v|
+      #the search
+      results << {:handler => v, :items => [1,2,3,4,5]} #dummy content
+    end
+    results
+  end
 end

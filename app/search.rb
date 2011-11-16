7e3382861a3c3651143996f514f1de91d3c50924
Rob McGrail <rob@cwa-1498.(none)>
2011-11-16 17:22:01 +1300

    controllers for rank etc

--- app/search.rb
+++ app/search.rb
@@ -1,5 +1,5 @@
 # http://datamapper.org/docs/properties.html
-# Note default values in settings.rb
+# Note default validations in settings.rb
 
 class Search
   include DataMapper::Resource
@@ -11,17 +11,18 @@ class Search
   property :b,            String
   property :winner,       String,   :required => false
   property :loser,        String,   :required => false
-  property :win_score,    Integer,  :required => false 
+  property :win_score,    Integer,  :default  => 0
   property :created_at,   DateTime
   
   
   def log(file_name = settings.solr_log_file)
     begin
       unless File.exists? file_name
-        File.open(file_name, w) {|f| f.write 'DATE, QUERY_TERM, A, B, WINNER, LOSER, SCORE'}
+        File.open(file_name, w) {|f| f.write 'DATE, IP, QUERY_TERM, A, B, WINNER, LOSER, SCORE'}
       end
       File.open(file_name, 'a+') do |f|
         f.write "#{self.created_at}, \
+        #{@env['REMOTE_ADDR']}, \
         #{self.query_term}, \
         #{self.a}, \
         #{self.b}, \
@@ -68,15 +69,32 @@ end
 
 
 post '/compare/rank?' do
-
-  # update search
-  # log results
+  @a = Handler.first(:name => params[:a]) #error handling needed, also how does this gel with Handlers.get_two...
+  @b = Handler.first(:name => params[:b])
+  assign_handler_points
+  @search = Search.first(:id => params[:search_id])
+  @search.attributes = {
+    :winner => params[:winner],
+    :loser => loser(@a, @b),
+    :win_score => @search.win_score + params[:score].to_i,
+  }
+  if @search.save
+    @search.log
+    flash[:success] = success_text[:ranked]
+    redirect '/'
+  else
+    flash[:error] = error_text[:generic]
+    redirect '/'
+  end
 end
 
 
 get '/search/:id?' do
-
-
+  @search = Search.first(:id => params[:id])
+  handlers = Handlers.get_pair_safely(@search.a, @search.b)
+  q = injest_query(@search.query_term)
+  @results = ab_search(handlers, q)
+  haml :'results/main'
 end
 
 

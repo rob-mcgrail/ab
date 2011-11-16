762f79559b870812d7499bb04029d6e1dc744ecc
Rob McGrail <rob@cwa-1498.(none)>
2011-11-16 17:22:41 +1300

    pulling stuff out of main search methods

--- helpers/search.rb
+++ helpers/search.rb
@@ -1,19 +1,13 @@
 helpers do
-  def handler_check(h)
-    if h == nil
-      flash[:error] = error_text[:no_handlers]
-      redirect '/'
-    end
-  end
-  
-  def injest_query
-    if params[:query_term] == nil || params[:query_term] == ''
+  def injest_query(q = params[:query_term])
+    if q == nil || q == ''
       flash[:error] = error_text[:no_query]
       redirect '/'
     end
-    params[:query_term]
+    q
   end
-  
+
+
   def ab_search(handlers, q)
     results = []
     handlers.each do |k,v|
@@ -21,5 +15,37 @@ helpers do
       results << {:handler => v, :items => [1,2,3,4,5]} #dummy content
     end
     results
+  end
+  
+  
+  def assign_handler_points
+    winner = params[:winner]
+    if @a.name == loser(@a, @b)
+      @a.won(params[:score]); @b.lost
+    else
+      @b.won(params[:score]); @a.lost
+    end
+    
+    unless @a.save
+      flash[:error] = error_text[:generic] #make these raise exceptions instead?
+      redirect '/'
+    end
+    
+    unless @b.save
+      flash[:error] = error_text[:generic] #make these raise exceptions instead?
+      redirect '/'
+    end
+  end
+  
+  
+  def loser(a,b)
+    if a.name == params[:winner]
+      b.name
+    elsif b.name == params[:winner]
+      a.name
+    else
+      flash[:error] = error_text[:generic] #make these raise exceptions instead?
+      redirect '/'    
+    end
   end
 end

605ae1a09289499bdcb595b2451394e37fed7208
Rob McGrail <rob@cwa-1498.(none)>
2011-11-16 15:43:27 +1300

    new error text

--- helpers/error_text.rb
+++ helpers/error_text.rb
@@ -1,9 +1,10 @@
 helpers do
   def error_text
     {
-      :no_handlers => 'Serious error: Search handlers not found!',
       :forbidden => 'You can\'t go there!',
       :generic => 'An error has occured',
+      :no_handlers => 'Serious error: Search handlers not found!',
+      :no_query => 'You need to enter a search term!',
     }
   end
 end

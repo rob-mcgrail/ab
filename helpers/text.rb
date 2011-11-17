cdcd0e1153ddd2c11e169c3c566bde5414bd0fdc
Rob McGrail <rob@cwa-1498.(none)>
2011-11-17 17:04:00 +1300

    new errors

--- helpers/text.rb
+++ helpers/text.rb
@@ -3,15 +3,18 @@ helpers do
     {
       :forbidden => 'You can\'t do that. How about a search?',
       :generic => 'An error has occured',
+      :search_not_found => 'You requested a previous search that doesn\'t exist!',
       :no_handlers => 'Serious error: Search handlers not found!',
       :missing_handler => 'Couldn\'t find something we need!',
+      :cant_save => 'Something went serious wrong!',
       :no_query => 'You need to enter a search term!',
     }
   end
   
   def success_text
     {
-      :ranked => "Thanks. Your vote has been saved. Retrieve your search @ #{link_to uri("search/#{@search.id}", true), "search/#{@search.id}"}"
+      :ranked => "Thanks. Your vote has been saved. Retrieve your search @ #{link_to uri("search/#{@search.id}", true), "search/#{@search.id}"}",
+      :search_for => "Results for #{safe @search.query_term}"
     }
   end
 end

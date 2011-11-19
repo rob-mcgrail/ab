34e6d81e283c9a9a01d45095c18c2d23718bac4c
Rob McGrail <rob@aichi.(none)>
2011-11-19 17:19:58 +1300

    rationalising settings

--- settings.rb
+++ settings.rb
@@ -1,15 +1,18 @@
+configure do
+  set :base_score, 1
+  set :super_score, 2
+  set :solr_log_file, 'public/log/searches.csv'
+  set :method_override, true
+  set :sessions, true
+  set :logging, false # stops annoying double log messages.
+end
+
 configure :development do
   set :db, 'sqlite3://' + settings.root + '/db/development.sqlite3'
   set :raise_errors, true
   set :show_exceptions, true
   set :static, false
-  set :logging, false # stops annoying double log messages.
-  set :method_override, true # probably not using this
-  set :sessions, true
   set :haml, {:format => :html5, :ugly => false }
-  set :solr_log_file, 'public/log/searches.csv'
-  set :base_score, 1
-  set :super_score, 2
 end
 
 configure :production do
@@ -17,13 +20,7 @@ configure :production do
   set :raise_errors, false
   set :show_exceptions, false
   set :static, false
-  set :logging, false # stops annoying double log messages.
-  set :method_override, true # probably not using this
-  set :sessions, true
   set :haml, {:format => :html5, :ugly => true }
-  set :solr_log_file, 'public/log/searches.csv'
-  set :base_score, 1
-  set :super_score, 2
 end
 
 # Database; 

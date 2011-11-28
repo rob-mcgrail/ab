be3191629fdcc458199f1ff5126b5653ef4f36af
Rob McGrail <rob@cwa-1498.(none)>
2011-11-28 16:45:57 +1300

    check query isn't injesting hint text

--- helpers/queries.rb
+++ helpers/queries.rb
@@ -1,6 +1,6 @@
 helpers do
   def injest_query(q)
-    if q == nil || q == ''
+    if q == nil || q == '' || q == misc_text[:search_box]
       flash[:error] = error_text[:no_query]
       redirect '/'
     end

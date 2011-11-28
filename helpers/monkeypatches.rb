fd5f8e2a94ef66b788d144114e07ba14b9f131df
Rob McGrail <rob@cwa-1498.(none)>
2011-11-28 16:45:17 +1300

    get rid of annoying object#id warning

--- helpers/monkeypatches.rb
+++ helpers/monkeypatches.rb
@@ -9,3 +9,12 @@ class Sinatra::Base
     settings
   end
 end
+
+# 1.8.7-head freaks out when it sees
+# calls to #id, which is annoying because
+# it's a perfectly normal datamapper method...
+
+class Object
+  def id
+  end
+end

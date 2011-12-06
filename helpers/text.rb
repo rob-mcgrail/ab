04da8db9b3c0e4c16d031b650660db6812b93cd7
Rob McGrail <rob@cwa-1498.(none)>
2011-12-06 14:08:15 +1300

    text additions

--- helpers/text.rb
+++ helpers/text.rb
@@ -9,7 +9,9 @@ helpers do
       :cant_save => 'Something went serious wrong!',
       :no_query => 'You need to enter a search term!',
       :need_handlers => 'You need at least two handlers!',
-      :invalid_handler => 'The handler you tried to create had invalid parameters!'
+      :invalid_handler => 'The handler you tried to create had invalid parameters!',
+      :invalid_password => 'Sorry, that\'s not the password',
+      :needs_password => 'Sorry, only administrators can see that...'   
     }
   end
   
@@ -27,7 +29,9 @@ helpers do
       :ranked => "Thanks. Your vote has been saved. Retrieve your search @ #{link_to uri("search/#{@search.id}", true), "search/#{@search.id}", :class => 'success'}",
       :search_for => "Results for #{safe @search.query_term}",
       :handler_state_changed => "#{@handler.name} changed to #{@state}.",
-      :new_handler => "#{@handler.name} created."
+      :new_handler => "#{@handler.name} created.",
+      :logged_out => 'You are now logged out.',
+      :logged_in => 'You are now logged in'
     }
   end
   

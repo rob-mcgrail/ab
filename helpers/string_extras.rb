206ebffbf6b1c4577f5cdf0cd3daf5e11652e8ae
Rob McGrail <rob@cwa-1498.(none)>
2011-12-08 11:15:03 +1300

    chop middle out of string, for displaying long urls

--- helpers/string_extras.rb
+++ helpers/string_extras.rb
@@ -12,4 +12,17 @@ class String
   def make_matchable
     BCrypt::Password.new(self)
   end
+  
+  def url_trim(i, delimeter = '...')
+    if self.length > i
+      x = (self.length/2).to_i
+      first = self[0, (x/2)-1]
+      first = first[0, (i/2)-1]
+      second = self[(x/2), self.length]
+      second = second[0, (i/2)-1]
+      first + delimeter + second
+    else
+      self
+    end
+  end
 end

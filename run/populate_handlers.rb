91bd569e8f5dca630006c85dbae5e5f26f529f21
Rob McGrail <rob@cwa-1498.(none)>
2011-12-12 16:46:27 +1300

    more handlers

--- run/populate_handlers.rb
+++ run/populate_handlers.rb
@@ -40,5 +40,13 @@ puts "Creating handler '#{handler.name}'"
 
 handler.save
 
+handler = Handler.first_or_create({ :name => 'Descripts' }, {
+  :name => 'Descripts',
+  :request => 'defType=dismax&ps=3&qf=title_t^10 keyword.text^10 description_t^10 location_t dc.format dc.right dc.subject topics.all.text contentprovider contentsource strand learningarea keylearningobjective educationalvalue host site institution_name_t url^40 ezf_df_text id&pf=title_t^10 description_t^20&qt=standard&qs=2&bq=url:tki^20 dc.right:"Ministry of Education Te Tāhuhu o te Mātauranga"^10&mm=30%&fq=meta_anon_access_b:true AND (url:[* TO *] AND url:http AND -url:cmis.cwa.co.nz AND -url:"http\://admin" AND -url:"http\://www.nzmaths.co.nz" AND -url:.internal. AND -url:.cwa.co.nz AND -url:/Media/Images/ AND -url:/Media/Files/) AND -keyword:waec AND -keyword:moec AND englishstatus:live',
+  :created_at => Time.now
+})
 
+puts "Creating handler '#{handler.name}'"
+
+handler.save
 
